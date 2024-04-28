655dcbddaf390a5a41b5d0e2a99d01a33c885679
Ian <ia.cortes@duocuc.cl>
2024-04-28 16:44:10 -0400

    (funcionando)agregado de producto a la tabla, falta poder eliminar

--- js/miscript.js
+++ js/miscript.js
@@ -1,8 +1,8 @@
 function agregarFila() {
-    let idproducto = document.getElementById("idproducto").value;
-    let nombre = document.getElementById("idnombreproducto").value;
-    let cantidad = document.getElementById("idcantidadproducto").value;
-    let precio = document.getElementById("idprecioproducto").value;
+    let idproducto = document.getElementById("IDproducto").value;
+    let nombre = document.getElementById("nombre").value;
+    let cantidad = document.getElementById("cantidad").value;
+    let precio = document.getElementById("precio").value;
     
     let table = document.getElementById("tablaproductos").getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.rows.length);
@@ -12,9 +12,11 @@ function agregarFila() {
     let cell4 = newRow.insertCell(3);
     cell1.innerHTML = idproducto;
     cell2.innerHTML = nombre;
-    cell3.innerHTML = cantidad;
-    cell4.innerHTML = precio
+    cell3.innerHTML = cantidad + " / 30";
+    cell4.innerHTML =  ("$" + precio);
+    cell5.innerHTML = '<button onclick="eliminarFila(this)">Eliminar</button>';
 
     // Limpiar el formulario después de agregar los datos a la tabla
     document.getElementById("idformagregadoproductos").reset();
 }
+
